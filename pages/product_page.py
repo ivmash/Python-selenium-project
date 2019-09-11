22498e80ad18a8ae4d96b83259cae59de29a6aa2
vakingit <vakin@protonmail.com>
2019-09-11 17:27:34 +0100

    functions rewrote with using inheritance

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,5 +1,8 @@
 from .base_page import BasePage
 from .locators import ProductPageLocator
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
 class ProductPage(BasePage):
@@ -7,12 +10,26 @@ class ProductPage(BasePage):
     def add_to_basket(self):
         self.browser.find_element(*ProductPageLocator.ADD_TO_BASKET_BUTTON).click()
 
-    def can_guest_see_confirmation_messages(self):
-        assert self.is_element_present(*ProductPageLocator.BASKET_MESSAGE_1),"First message is not present"
+    def should_not_be_success_messages(self):
+        WebDriverWait(self.browser, 5).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "div[id='messages'] > div:nth-child(1)")))
+        assert self.is_element_present(*ProductPageLocator.SUCCESS_MESSAGE), "First message is not present"
         assert self.is_element_present(*ProductPageLocator.BASKET_MESSAGE_2), "Second message is not present"
-        message = self.browser.find_element(*ProductPageLocator.BASKET_MESSAGE_1_PRODUCT_NAME).text
+
+    def should_be_equal_names(self):
+        message = self.browser.find_element(*ProductPageLocator.SUCCESS_MESSAGE_PRODUCT_NAME).text
         product_name = self.browser.find_element(*ProductPageLocator.PRODUCT_NAME).text
         assert message == product_name, f"Product names is not equal,{message},{product_name}"
-        sum = self.browser.find_element(*ProductPageLocator.BASKET).text.split()[2]
+
+    def should_be_equal_amounts(self):
+        amount = self.browser.find_element(*ProductPageLocator.BASKET).text.split()[2]
         price = self.browser.find_element(*ProductPageLocator.PRODUCT_PRICE).text
-        assert sum == price, f"Basket sum and product price are not equal,{sum},{price}"
+        assert amount == price, f"Basket sum and product price are not equal,{amount},{price}"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocator.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_not_be_success_message_is_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocator.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,7 @@ from pages.product_page import ProductPage
 import time
 import pytest
 
+
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -12,9 +13,38 @@ import pytest
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_basket(browser,link):
-    page = ProductPage(browser,link)
+def test_basket(browser, link):
+    page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-    page.can_guest_see_confirmation_messages()
+    page.should_not_be_success_messages()
+    page.should_be_equal_names()
+    page.should_be_equal_amounts()
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message_is_disappeared()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
